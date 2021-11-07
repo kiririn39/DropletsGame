e6818eec5ebc52af232f42ea1ad04ddae057c55d
Karooto <vladboroday@gmail.com>
2021-11-08 00:49:57 +0200

    fixed droplets spawn positions

--- Assets/Project/Source/TransitBoundsCalculator.cs
+++ Assets/Project/Source/TransitBoundsCalculator.cs
@@ -20,13 +20,19 @@ namespace Project.Source
         {
             Vector3 topLeft = _camera.ViewportToWorldPoint(new Vector3(0, 1, 0));
             Vector3 topRight = _camera.ViewportToWorldPoint(new Vector3(1, 1, 0));
+            topLeft.z = 0;
+            topRight.z = 0;
+
             return Vector3.Lerp(topLeft, topRight, _random.NextFloat());
         }
 
         public Vector3 TransitEndPoint()
         {
-            Vector3 bottomLeft = _camera.ViewportToWorldPoint(new Vector3(0, 0, 0));
-            Vector3 bottomRight = _camera.ViewportToWorldPoint(new Vector3(1, 0, 0));
+            Vector3 bottomLeft = _camera.ViewportToWorldPoint(new Vector3(0, 0, _camera.nearClipPlane));
+            Vector3 bottomRight = _camera.ViewportToWorldPoint(new Vector3(1, 0, _camera.nearClipPlane));
+            bottomLeft.z = 0;
+            bottomRight.z = 0;
+
             return Vector3.Lerp(bottomLeft, bottomRight, _random.NextFloat());
         }
     }
