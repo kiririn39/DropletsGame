d606dd5a6e6c77b176ea6e27bcb87e98811dbf39
Karooto <vladboroday@gmail.com>
2021-12-08 13:02:04 +0200

    added droplets extensions. Added scaler extension

---
+++ Assets/Project/Source/DropletsExtensions/DropletExtension.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Project.Source.DropletsExtensions
+{
+    public abstract class DropletExtension : MonoBehaviour
+    {
+        protected Droplet Droplet;
+
+
+        public abstract void DoUpdate(float deltaTime, float transitProgress);
+        public virtual void Extend(Droplet droplet) => Droplet = droplet;
+        public abstract void TransitStarted(Vector3 from, Vector3 to);
+        public abstract void TransitFinished();
+        public abstract void TransitInterrupted();
+    }
+}
---
+++ Assets/Project/Source/DropletsExtensions/ScaleDropletOverTime.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace Project.Source.DropletsExtensions
+{
+    public class ScaleDropletOverTime : DropletExtension
+    {
+        [SerializeField] private AnimationCurve uniformScalingCurve;
+
+
+        public override void DoUpdate(float deltaTime, float transitProgress)
+        {
+            var scale = uniformScalingCurve.Evaluate(transitProgress) * Vector3.one;
+
+            Droplet.transform.localScale = scale;
+        }
+
+        public override void TransitStarted(Vector3 @from, Vector3 to)
+        {
+        }
+
+        public override void TransitFinished()
+        {
+        }
+
+        public override void TransitInterrupted()
+        {
+        }
+    }
+}
