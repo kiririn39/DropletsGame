e4bd19c6098369246193eedc2fd8b61de539352a
Karooto <vladboroday@gmail.com>
2021-12-08 13:03:32 +0200

    Updated droplet script to utilize droplets extensions

--- Assets/Project/Source/Droplet.cs
+++ Assets/Project/Source/Droplet.cs
@@ -1,4 +1,5 @@
 using System;
+using Project.Source.DropletsExtensions;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
@@ -8,14 +9,21 @@ namespace Project.Source
     public class Droplet : MonoBehaviour, IUpdatable, IPointerClickHandler
     {
         [SerializeField] private float movementSpeed;
+        [SerializeField] private DropletExtension[] _extensions;
         private Vector3 _moveFrom;
         private Vector3 _moveTo;
         private float _movementProgress;
 
-        private Action _transitInterrupted;
-        private Action _transited;
+        private Action<Droplet> _transitInterrupted;
+        private Action<Droplet> _transited;
         private Action<Droplet> _lifecycleEnded;
 
+
+        private void Awake()
+        {
+            foreach (var dropletExtension in _extensions)
+                dropletExtension.Extend(this);
+        }
 
         public void Transit(Vector3 from, Vector3 to)
         {
@@ -23,6 +31,9 @@ namespace Project.Source
             transform.position = _moveFrom;
             _moveFrom = from;
             _moveTo = to;
+
+            foreach (var dropletExtension in _extensions)
+                dropletExtension.TransitStarted(from, to);
         }
 
         public void DoUpdate(float deltaTime)
@@ -30,23 +41,38 @@ namespace Project.Source
             _movementProgress += movementSpeed * deltaTime;
             transform.position = Vector3.Lerp(_moveFrom, _moveTo, _movementProgress);
 
+            foreach (var dropletExtension in _extensions)
+                dropletExtension.DoUpdate(deltaTime, _movementProgress);
+
             if (_movementProgress < 1.0f)
                 return;
 
             InvokeLifecycleEnd(_transited);
         }
 
-        public void OnTransitFinished(Action action) => _transited = action;
+        public void OnTransitFinished(Action<Droplet> action)
+        {
+            foreach (var dropletExtension in _extensions)
+                dropletExtension.TransitFinished();
 
-        public void OnTransitInterrupted(Action action) => _transitInterrupted = action;
+            _transited = action;
+        }
+
+        public void OnTransitInterrupted(Action<Droplet> action)
+        {
+            foreach (var dropletExtension in _extensions)
+                dropletExtension.TransitInterrupted();
+
+            _transitInterrupted = action;
+        }
 
         public void OnLifecycleEndAction(Action<Droplet> action) => _lifecycleEnded = action;
 
         public void OnPointerClick(PointerEventData eventData) => InvokeLifecycleEnd(_transitInterrupted);
 
-        protected virtual void InvokeLifecycleEnd(Action preEndAction)
+        protected virtual void InvokeLifecycleEnd(Action<Droplet> preEndAction)
         {
-            preEndAction?.Invoke();
+            preEndAction?.Invoke(this);
             _lifecycleEnded?.Invoke(this);
         }
     }
